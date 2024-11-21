24621d6bf680bf61de3b6590f2fb9bfdf2c4281b
shaileshsuresh <47384788+shaileshsuresh@users.noreply.github.com>
2024-11-22 00:16:42 +0100

    Update newhp_filter.vhdl

--- Filters/newhp_filter.vhdl
+++ Filters/newhp_filter.vhdl
@@ -1,135 +1,134 @@
--- EQUALIZER: high-pass filter component
+--This module checks the filter order, sampling frequency and cut-off frequency for the filter and filters out the noise signal from the input using the filter coefficients 
+--computed in hp_part.vhdl component.
 
 library ieee;
-  use ieee.std_logic_1164.all;
-  use ieee.numeric_std.all;
-  
-entity newhp_filter is
-  port (
-    clk    : in  std_logic;
-    rstn   : in  std_logic;
-    fclk   : in  std_logic; --  frame clock 48000Hz
-    y      : out std_logic_vector(15 downto 0);
-    x      : in  std_logic_vector(15 downto 0)
-  );
-end entity newhp_filter;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
 
-architecture arch_newhp_filter of newhp_filter is
-  type para_array_type is array(0 TO 4) OF std_logic_vector(27 downto 0);
-  -- B0-B1-B2-A1-A2
-  constant PART1 : para_array_type := ( x"0100000", -- 1
-                                        x"FF00038", -- -0.999946810631343
-                                        x"0000000", -- 0
-                                        x"FF9145F", -- -0.995027187519884
-                                        x"0000000"  -- 0
-                                      );
-  constant PART2 : para_array_type := ( x"0100000", -- 1
-                                        x"FDFFFAC", -- -2.000080735080919
-                                        x"010005A", -- 1.000086065884170
-                                        x"FE02107", -- -1.991936828367649
-                                        x"00FDF18"  -- 0.991966884654222
-                                      );
-  constant PART3 : para_array_type := ( x"0100000", -- 1
-                                        x"FE00032", -- -1.999953177722835
-                                        x"00FFFDD", -- 0.999967129142807
-                                        x"FE00CC3", -- -1.996885171284679
-                                        x"00FF366"  -- 0.996923908852947
-                                      );
-  constant G : std_logic_vector(27 downto 0) :=x"00FDF18";-- x""; -- 0.991966638925721
+
+entity hp_filter is
+  generic (
+    N   : integer   := 4;                           -- filter order
+    FS  : std_logic_vector(15 downto 0) := x"BB80"; -- sampling frequency
+    DFC : std_logic_vector(15 downto 0) := x"0064"  -- default cut-off frequency
+  );
+  port (
+    clk        : in std_logic;
+    fclk       : in std_logic;
+    rstn       : in std_logic;
+    -- from filter controller
+    fcon_en_i  : in std_logic;
+    fcon_cmd_i : in std_logic_vector(15 downto 0);
+    -- data
+    in_sig     : in  std_logic_vector(15 downto 0);
+    out_sig    : out std_logic_vector(15 downto 0)
+  );
+end entity hp_filter;
+
+architecture arch_hp_filter of hp_filter is
+  constant PI : signed(2 downto -13) := x"647A";
+  -- temp
+  signal fc : std_logic_vector(15 downto 0);
   -- component
-  component eq_part is
+  component hp_part is
     generic (
-      B0 : std_logic_vector(27 downto 0); -- 7:-20
-      B1 : std_logic_vector(27 downto 0);
-      B2 : std_logic_vector(27 downto 0);
-      A1 : std_logic_vector(27 downto 0);
-      A2 : std_logic_vector(27 downto 0)
+      FS : std_logic_vector(15 downto 0):=x"BB80"; -- sampling frequency
+      COS: std_logic_vector(23 downto 0):=x"FFFBDC" -- cos(ki) [11:-12]
     );
     port (
       clk    : in  std_logic;
       fclk   : in  std_logic; --  frame clock 48000Hz
       rstn   : in  std_logic;
-      y      : out std_logic_vector(35 downto 0);
-      x      : in  std_logic_vector(35 downto 0)
+      cordic : in  std_logic_vector(31 downto 0);
+      y      : out std_logic_vector(15 downto 0);
+      x      : in  std_logic_vector(15 downto 0)
     );
-  end component eq_part;
+  end component hp_part;
+  -- cordic ip
+  COMPONENT cordic_sin
+    PORT (
+      aclk                : IN STD_LOGIC;
+      s_axis_phase_tvalid : IN STD_LOGIC;
+      s_axis_phase_tdata  : IN STD_LOGIC_VECTOR(15 DOWNTO 0);
+      m_axis_dout_tvalid  : OUT STD_LOGIC;
+      m_axis_dout_tdata   : OUT STD_LOGIC_VECTOR(31 DOWNTO 0) 
+    );
+  END COMPONENT;
+  -- cordic
+  signal cordic_cal_1 : signed(15 downto -16);
+  signal cordic_cal_2 : signed(5 downto -26);
+  signal cordic_input : signed(2 downto -13);--
+  signal cordic_output: STD_LOGIC_VECTOR(31 downto 0);
+
+  type cos_array_type is array(1 TO N/2) OF std_logic_vector(23 downto 0);
+  type data_array_type is array(1 TO N/2) OF std_logic_vector(15 downto 0);
+  constant COS : cos_array_type := (x"FFF9e1",
+                                    x"FFF138");
+  signal y_signal : data_array_type;
   
-  signal eq_part1_out  : std_logic_vector(35 downto 0);
-  signal eq_part2_out  : std_logic_vector(35 downto 0);
-  signal eq_part3_out  : std_logic_vector(35 downto 0);
-
-  signal eq_part1_gain : signed(63 downto 0);
-  signal eq_part1_in  : std_logic_vector(35 downto 0);
-  signal eq_part2_in   : std_logic_vector(35 downto 0);
-
 begin
-  eq_part1_in <= (std_logic_vector'(x"00") & x & std_logic_vector'(x"000")) when x(15) = '0' else (std_logic_vector'(x"FF") & x & std_logic_vector'(x"000"));
-
-  inst_eq_part1:
-  component eq_part
+  -- temp
+  -- calculate pi*fc/fs -> cordic_input
+  cordic_cal_1 <= signed(std_logic_vector'(fc & x"0000")) / signed(std_logic_vector'(x"0000" & FS)); 
+  cordic_cal_2 <= cordic_cal_1(2 downto -13) * PI;
+  cordic_input <= cordic_cal_2(2 downto -13);
+  -- main
+  out_sig <= y_signal(2);
+  
+  inst_hp_part_1:
+    hp_part 
     generic map (
-      B0 => PART1(0),
-      B1 => PART1(1),
-      B2 => PART1(2),
-      A1 => PART1(3),
-      A2 => PART1(4)
+      FS => FS,
+      COS => COS(1)
     )
     port map (
-      clk  => clk,
-      fclk => fclk,
-      rstn => rstn,
-      x    => eq_part1_in,
-      y    => eq_part1_out
+      clk    => clk,
+      fclk   => fclk,
+      rstn   => rstn,
+      cordic => cordic_output,
+      y      => y_signal(1),
+      x      => in_sig
     );
-
-  eq_part1_gain <= signed(eq_part1_out) * signed(G);
+  --gene_hpf:
+  --for i in 2 to N/2 generate
+    inst_hp_part_i:
+    hp_part 
+    generic map (
+      FS => FS,
+      COS => COS(2)
+    )
+    port map (
+      clk    => clk,
+      fclk   => fclk,
+      rstn   => rstn,
+      cordic => cordic_output,
+      y      => y_signal(2),
+      x      => y_signal(1)
+    );
+  --end generate gene_hpf;
+  -- cordic ip
+  inst_cordic_sin:
+  cordic_sin
+  port map (
+    aclk                => fclk,
+    s_axis_phase_tvalid => '0',
+    s_axis_phase_tdata  => std_logic_vector(cordic_input),
+    m_axis_dout_tdata   => cordic_output
+  );
   
-  gain_proc:
-  process (rstn,fclk)
+  fc_update_proc:
+  process (clk,rstn)
   begin
     if rstn = '0' then
-      eq_part2_in <= x"000000000";
-    elsif rising_edge(fclk) then
-      eq_part2_in <= std_logic_vector(eq_part1_gain(55 downto 20));
+      fc <= DFC;
+    elsif rising_edge(clk) then
+      if fcon_en_i = '1' then
+        fc <= fcon_cmd_i;
+      else
+        fc <= fc;
+      end if;
     end if;
-  end process gain_proc;
-
-
-
-  inst_eq_part2:
-  component eq_part
-    generic map (
-      B0 => PART2(0),
-      B1 => PART2(1),
-      B2 => PART2(2),
-      A1 => PART2(3),
-      A2 => PART2(4)
-    )
-    port map (
-      clk  => clk,
-      fclk => fclk,
-      rstn => rstn,
-      x    => eq_part2_in,
-      y    => eq_part2_out
-    );
-
-  inst_eq_part3:
-  component eq_part
-    generic map (
-      B0 => PART3(0),
-      B1 => PART3(1),
-      B2 => PART3(2),
-      A1 => PART3(3),
-      A2 => PART3(4)
-    )
-    port map (
-      clk  => clk,
-      fclk => fclk,
-      rstn => rstn,
-      x    => eq_part2_out,
-      y    => eq_part3_out
-    );
-
-  y <= eq_part3_out(27 downto 12);
+  end process fc_update_proc;
   
-end architecture arch_newhp_filter;
+
+end arch_hp_filter;
